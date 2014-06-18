008d1f7c92702e4aeb3964b1c39e9ca12145c8fa
chenxk <826179140@qq.com>
2014-06-18 15:18:36 +0800

    TTT

---
+++ TestDemo/src/com/test/TTT.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class TTT {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("init data");
+	}
+
+}
