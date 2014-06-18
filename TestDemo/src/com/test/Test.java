a3afa02b8e60f9020fdd3282da91a84205919075
chenxk <826179140@qq.com>
2014-06-18 14:57:09 +0800

    22222

--- TestDemo/src/com/test/Test.java
+++ TestDemo/src/com/test/Test.java
@@ -4,7 +4,6 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		System.out.println("abcdsfs");
 		System.out.println("sfsfsdf");
 		System.out.println("sdfsfdf");
