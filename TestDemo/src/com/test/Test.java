daf36984ab1198cd3e41a95c9c4f6dd2db8e8fd0
chenxk <826179140@qq.com>
2014-06-18 14:43:55 +0800

    abab

--- TestDemo/src/com/test/Test.java
+++ TestDemo/src/com/test/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("abcdsfs");
+		
 		System.out.println("sdfsfdf");
 		System.out.println("it is cool!");
 	}
