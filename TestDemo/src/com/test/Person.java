a230d878f607a343016e62ed3814f0f98ba76f8f
chenxk <826179140@qq.com>
2014-06-18 18:03:46 +0800

    name

--- TestDemo/src/com/test/Person.java
+++ TestDemo/src/com/test/Person.java
@@ -3,6 +3,17 @@ package com.test;
 public class Person {
 
 	private int age;
+	
+	private String name;
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	public int getAge() {
 		return age;
